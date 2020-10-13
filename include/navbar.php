c4522df28607dfb8bd8957f5ab59ff8194746920
unknown <anhdung.dau@gmail.com>
2020-10-13 12:20:43 +1100

    adding navigation bar for multiple uses

--- include/navbar.php
+++ include/navbar.php
@@ -1,13 +1,24 @@
-<div class="w3-container">
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="index.php">AFPC</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li><a href="../registration/index.php">Presentations List</a></li>
+            <li><a href="../content/showSpeakers.php">Speakers List</a></li>
+            <li><a href="#">Venue List</a></li>
+        </ul>
 
-<h2>Right-Align Links</h2>
-<p>To right-align elements in the navigation bar, add the w3-right class:</p>
-
-<div class="w3-bar w3-border w3-green">
-  <a href="#" class="w3-bar-item w3-button">Home</a>
-  <a href="#" class="w3-bar-item w3-button">Link 1</a>
-  <a href="#" class="w3-bar-item w3-button">Link 2</a>
-  <a href="#" class="w3-bar-item w3-button w3-right">Link 3</a>
-</div>
-
-</div>
+        <div class="nav navbar-right">
+            <!-- notification message -->
+            <?php if (isset($_SESSION['success'])) : ?>
+                <?php endif ?>
+                    <?php  if (isset($_SESSION['user'])) : ?>
+                        <strong><?php echo $_SESSION['user']['username']; ?></strong>
+                        <i style="color: green;"><?php echo ucfirst($_SESSION['user']['user_type']); ?></i>
+                        <br>
+                        <?php endif ?>
+                            <small><a href="index.php?logout='1'" style="color: red;">Logout</a></small>
+        </div>
+    </div>
+</nav>
