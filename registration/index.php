b99b8bea609d99d3e1f98e9e67f849b6e734c0fc
unknown <anhdung.dau@gmail.com>
2020-10-13 12:21:54 +1100

    update user's homepage

--- registration/index.php
+++ registration/index.php
@@ -1,71 +1,35 @@
 <?php 
 	include('functions.php');
-
 	if (!isLoggedIn()) {
 		$_SESSION['msg'] = "You must log in first";
 		header('location: login.php');
 	}
 ?>
+
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Home</title>
-	<link rel="stylesheet" type="text/css" href="../css/user.css">
-</head>
-<body>
-	<div class="header">
-		<h2>Home Page</h2>
-	</div>
-	<div class="content">
-		<!-- notification message -->
-		<?php if (isset($_SESSION['success'])) : ?>
-			<div class="error success" >
-				<h3>
-					<?php 
-						echo $_SESSION['success']; 
-						unset($_SESSION['success']);
-					?>
-				</h3>
-			</div>
-		<?php endif ?>
-		<!-- logged in user information -->
-		<div class="profile_info">
-			<img src="images/user_profile.png"  >
-
-			<div>
-				<?php  if (isset($_SESSION['user'])) : ?>
-					<strong><?php echo $_SESSION['user']['username']; ?></strong>
-
-					<small>
-						<i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i> 
-						<br>
-						<a href="index.php?logout='1'" style="color: red;">logout</a>
-					</small>
-
-				<?php endif ?>
-			</div>
-		</div>
-	</div>
-	<div class="container">
-    
-	    <h1>Welcome to the Australian Financial Pathways Conference (AFPC)</h1>
-                    <div>
-                        <h2>Timetable of Presentations</h2></div>
-                    <table id="table">
-                        <tr>
-                            <th>ID</th>
-                            <th>Title</th>
-                            <th>Speaker Name</th>
-                            <th>Company</th>
-                            <th>Start Time</th>
-                            <th>Duration</th>
-                            <th>Venue</th>
-                            <th>Location</th>
-                            <th>Capacity</th>
-                        </tr>
-                        <?php 
+    <?php include('../include/header.php'); ?>
+        <body>
+            <?php include('../include/navbar.php'); ?>
+                <h1>Welcome to the Australian Financial Pathways Conference (AFPC)</h1>
+                <br>
+                <div>
+                    <h3>Timetable of Presentations</h3></div>
+                <table id="table">
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Speaker Name</th>
+                        <th>Company</th>
+                        <th>Start Time</th>
+                        <th>Duration</th>
+                        <th>Venue</th>
+                        <th>Location</th>
+                        <th>Capacity</th>
+                    </tr>
+                    <?php 
                 $db = mysqli_connect('localhost', 'root', '', 'conference'); 
-                $sql = "SELECT * FROM showpre";
+                $sql = "SELECT * FROM showpre ORDER BY presentationID";
 //                $sql = "SELECT presentations.presentationID, topics.title, CONCAT(speakers.firstName,'',speakers.lastName) AS 'Speaker', speakers.company AS 'Company', presentations.startTime, presentations.duration, venues.title AS 'Venue', venues.location, venues.capacity FROM presentations
 //                INNER JOIN topics ON presentations.topicID = topics.topicID
 //                INNER JOIN speakers ON presentations.speakerID = speakers.speakerID
@@ -73,40 +37,39 @@
                 $result = mysqli_query($db,$sql);
                 while ($row=mysqli_fetch_array($result)) {                    
             ?>
-                            <tr>
-                                <td>
-                                    <?php echo $row['presentationID'];?>
-                                </td>
-                                <td>
-                                    <?php echo $row['title'];?>
-                                </td>
-                                <td>
-                                    <?php echo $row['Speaker'];?>
-                                </td>
-                                <td>
-                                    <?php echo $row['Company'];?>
-                                </td>
-                                <td>
-                                    <?php echo $row['startTime'];?>
-                                </td>
-                                <td>
-                                    <?php echo $row['duration'];?>
-                                </td>
-                                <td>
-                                    <?php echo $row['Venue'];?>
-                                </td>
-                                <td>
-                                    <?php echo $row['location'];?>
-                                </td>
-                                <td>
-                                    <?php echo $row['capacity'];?>
-                                </td>
-                            </tr>
-                            <?php 
+                        <tr>
+                            <td>
+                                <?php echo $row['presentationID'];?>
+                            </td>
+                            <td>
+                                <?php echo $row['title'];?>
+                            </td>
+                            <td>
+                                <?php echo $row['Speaker'];?>
+                            </td>
+                            <td>
+                                <?php echo $row['Company'];?>
+                            </td>
+                            <td>
+                                <?php echo $row['startTime'];?>
+                            </td>
+                            <td>
+                                <?php echo $row['duration'];?>
+                            </td>
+                            <td>
+                                <?php echo $row['Venue'];?>
+                            </td>
+                            <td>
+                                <?php echo $row['location'];?>
+                            </td>
+                            <td>
+                                <?php echo $row['capacity'];?>
+                            </td>
+                        </tr>
+                        <?php 
         }
         mysqli_close($db);  
         ?>
-                    </table>
-	</div>
-</body>
+                </table>
+        </body>
 </html>
