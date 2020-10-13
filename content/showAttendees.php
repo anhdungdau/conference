216daeaae92f09577130d5725e46a99d3a445312
unknown <anhdung.dau@gmail.com>
2020-10-13 12:40:23 +1100

    show list of attendees

---
+++ content/showAttendees.php
@@ -0,0 +1,53 @@
+<?php include('../include/session.php'); ?>
+
+<!DOCTYPE html>
+<html>
+    <?php include('../include/header.php'); ?>
+        <body>
+            <?php include('../include/navbar.php'); ?>
+                <h1>Welcome to the Australian Financial Pathways Conference (AFPC)</h1>
+                <br>
+                <div>
+                    <h3>Attendees List</h3></div>
+                <table id="table">
+                    <tr>
+                        <th>ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Company</th>
+                    </tr>
+                    <?php 
+                        $db = mysqli_connect('localhost', 'root', '', 'conference'); 
+                        $sql = "SELECT attendeeID, firstName, lastName, email, phone, company FROM attendees ORDER BY attendeeID";
+                        $result = mysqli_query($db,$sql);
+                        while ($row=mysqli_fetch_array($result)) {                    
+                    ?>
+                        <tr>
+                            <td>
+                                <?php echo $row['attendeeID'];?>
+                            </td>
+                            <td>
+                                <?php echo $row['firstName'];?>
+                            </td>
+                            <td>
+                                <?php echo $row['lastName'];?>
+                            </td>
+                            <td>
+                                <?php echo $row['email'];?>
+                            </td>
+                            <td>
+                                <?php echo $row['phone'];?>
+                            </td>
+                            <td>
+                                <?php echo $row['company'];?>
+                            </td>
+                        </tr>
+                        <?php 
+                            }
+                            mysqli_close($db);  
+                        ?>
+                </table>
+        </body>
+</html>
