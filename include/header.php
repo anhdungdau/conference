2a9c28339d81a012233315431d2bcd7729c37e98
unknown <anhdung.dau@gmail.com>
2020-10-13 12:20:14 +1100

    adding header for multiple uses

--- include/header.php
+++ include/header.php
@@ -1,41 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
     <meta charset="utf-8">
-    <title>Australian Financial Pathways Conference</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style/user.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bookshelf</title>
+    <link rel="stylesheet" href="../css/user.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
-
-<body>
-<!--
-    <header>
-        <h1>Australian Financial Pathways Conference</h1>
-        <p>Presented by GTB Financial Solutions
-            <br>20 September 2017
-            <br>Newcastle NSW Australia</p>
-    </header>
--->
-
-    <nav>
-        <div class="w3-container">
-            <div class="w3-bar w3-border w3-green">
-                <a href="index.php" class="w3-bar-item w3-button">Home</a>
-                <a href="#" class="w3-bar-item w3-button">Link 1</a>
-                <a href="#" class="w3-bar-item w3-button">Link 2</a>
-                <a href="#" class="w3-bar-item w3-button w3-right">User Logo</a>
-                <a href="#" class="w3-bar-item w3-button w3-right">User Profile</a>
-
-            </div>
-        </div>
-    </nav>
-
-
-
-    <main>
