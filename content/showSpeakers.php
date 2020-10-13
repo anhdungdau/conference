43ccf216bbe9bc93e7e82f2962ad1596609b698a
unknown <anhdung.dau@gmail.com>
2020-10-13 13:05:04 +1100

    add header and navbar

--- content/showSpeakers.php
+++ content/showSpeakers.php
@@ -1,10 +1,4 @@
-<?php 
-	include('../registration/functions.php');
-	if (!isLoggedIn()) {
-		$_SESSION['msg'] = "You must log in first";
-		header('location: login.php');
-	}
-?>
+<?php include('../include/session.php'); ?>
 
 <!DOCTYPE html>
 <html>
