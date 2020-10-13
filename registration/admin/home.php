419d95a5e793b59447c70db947cef737dfd507f6
unknown <anhdung.dau@gmail.com>
2020-10-13 13:03:44 +1100

    Home page for admin

--- registration/admin/home.php
+++ registration/admin/home.php
@@ -1,65 +1,42 @@
 <?php 
 	include('../functions.php');
-
 	if (!isAdmin()) {
 		$_SESSION['msg'] = "You must log in first";
 		header('location: ../login.php');
 	}
-
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Home</title>
-	<link rel="stylesheet" type="text/css" href="../style.css">
-	<style>
-	.header {
-		background: #003366;
-	}
-	button[name=register_btn] {
-		background: #003366;
-	}
-	</style>
-</head>
-<body>
-	<div class="header">
-		<h2>Admin - Home Page</h2>
-	</div>
-	<div class="content">
-		<!-- notification message -->
-		<?php if (isset($_SESSION['success'])) : ?>
-			<div class="error success" >
-				<h3>
-					<?php 
-						echo $_SESSION['success']; 
-						unset($_SESSION['success']);
-					?>
-				</h3>
-			</div>
-		<?php endif ?>
 
-		<!-- logged in user information -->
-		<div class="profile_info">
-			<img src="../images/admin_profile.png"  >
+    <!DOCTYPE html>
+    <html>
 
-			<div>
-				<?php  if (isset($_SESSION['user'])) : ?>
-					<strong><?php echo $_SESSION['user']['username']; ?></strong>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>AdminHomePage</title>
+        <link rel="stylesheet" type="text/css" href="../style.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    </head>
 
-					<small>
-						<i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i> 
-						<br>
-						<a href="home.php?logout='1'" style="color: red;">logout</a>
-						&nbsp; <a href="create_user.php"> + add user</a>
-					</small>
+    <body>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="nav navbar-left">
+                    <h1>Admin - Home Page</h1>
+                </div>
+                <div class="nav navbar-right">
+                    <?php  if (isset($_SESSION['user'])) : ?>
+                        <strong><?php echo $_SESSION['user']['username']; ?></strong>
+                        <i style="color: green;"><?php echo ucfirst($_SESSION['user']['user_type']); ?></i>
+                        <br>
+                        <?php endif ?>
+                            <small><a href="home.php?logout='1'" style="color: red;">Logout</a>&nbsp;</small>
+                </div>
+            </div>
+        </nav>
 
-				<?php endif ?>
-			</div>
-		</div>
+    </body>
 
-
-
-	</div>
-		
-</body>
-</html>
+    </html>
